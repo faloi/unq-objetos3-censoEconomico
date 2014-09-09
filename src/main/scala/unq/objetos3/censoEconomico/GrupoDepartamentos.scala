a866434691290837f61bff451886586065cbe86d
Federico Aloi <federico.aloi@gmail.com>
2014-09-09 18:58:47 -0300

    GrupoDepartamentos sabe si incluye a una provincia

--- src/main/scala/unq/objetos3/censoEconomico/GrupoDepartamentos.scala
+++ src/main/scala/unq/objetos3/censoEconomico/GrupoDepartamentos.scala
@@ -1,5 +1,7 @@
 package unq.objetos3.censoEconomico
 
 class GrupoDepartamentos(val departamentos: Iterable[Departamento]) {
+  def incluyeA(provincia: String) = departamentos.exists(_.provincia == provincia)
+
   def esHomogeneo = departamentos.map(_.provincia).toSet.size == 1
 }
--- src/test/scala/unq/objetos3/censoEconomico/GrupoDepartamentosSpec.scala
+++ src/test/scala/unq/objetos3/censoEconomico/GrupoDepartamentosSpec.scala
@@ -1,13 +1,22 @@
 package unq.objetos3.censoEconomico
 
 class GrupoDepartamentosSpec extends UnitSpec {
+  val departamentosCaba = new GrupoDepartamentos(Seq(new Departamento("Flores", "CABA"), new Departamento("Almagro", "CABA")))
+
   "Un grupo de departamentos" should "ser homogeneo si todos los departamentos pertenecen a la misma provincia" in {
-    new GrupoDepartamentos(Seq(new Departamento("Flores", "CABA"), new Departamento("Almagro", "CABA")))
-      .esHomogeneo should be (true)
+    departamentosCaba.esHomogeneo should be (true)
   }
 
   it should "no ser homogeneo si esta compuesto por departamentos de distintas provincias" in {
     new GrupoDepartamentos(Seq(new Departamento("Rosario", "Santa Fe"), new Departamento("Almagro", "CABA")))
       .esHomogeneo should be (false)
+  }
+
+  it should "poder decir si incluye una provincia" in {
+    departamentosCaba.incluyeA("CABA") should be (true)
+  }
+
+  it should "poder decir si no incluye una provincia" in {
+    departamentosCaba.incluyeA("Santa Fe") should be (false)
   }
 }
