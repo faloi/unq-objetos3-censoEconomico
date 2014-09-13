b2442148f01653f43863b3ab187e618b5d72a4fc
Federico Aloi <federico.aloi@gmail.com>
2014-09-12 21:32:11 -0300

    Estadisticas por Grupo de Departamento

--- src/main/scala/unq/objetos3/censoEconomico/GrupoDepartamentos.scala
+++ src/main/scala/unq/objetos3/censoEconomico/GrupoDepartamentos.scala
@@ -1,7 +1,9 @@
 package unq.objetos3.censoEconomico
 
-class GrupoDepartamentos(val departamentos: Iterable[Departamento]) {
+class GrupoDepartamentos(val departamentos: Set[Departamento]) extends Estadistica {
   def incluyeA(provincia: Provincia) = departamentos.exists(_.provincia == provincia)
 
   def esHomogeneo = departamentos.map(_.provincia).toSet.size == 1
+
+  override val criterio: (Registro) => Boolean = departamentos contains _.departamento
 }
--- src/test/scala/unq/objetos3/censoEconomico/GrupoDepartamentosSpec.scala
+++ src/test/scala/unq/objetos3/censoEconomico/GrupoDepartamentosSpec.scala
@@ -1,14 +1,16 @@
 package unq.objetos3.censoEconomico
 
+import org.joda.time.LocalDate
+
 class GrupoDepartamentosSpec extends UnitSpec {
-  val departamentosCaba = new GrupoDepartamentos(Seq(new Departamento("Flores", "CABA"), new Departamento("Almagro", "CABA")))
+  val departamentosCaba = new GrupoDepartamentos(Set(new Departamento("Flores", "CABA"), new Departamento("Almagro", "CABA")))
 
   "Un grupo de departamentos" should "ser homogeneo si todos los departamentos pertenecen a la misma provincia" in {
     departamentosCaba.esHomogeneo should be (true)
   }
 
   it should "no ser homogeneo si esta compuesto por departamentos de distintas provincias" in {
-    new GrupoDepartamentos(Seq(new Departamento("Rosario", "Santa Fe"), new Departamento("Almagro", "CABA")))
+    new GrupoDepartamentos(Set(new Departamento("Rosario", "Santa Fe"), new Departamento("Almagro", "CABA")))
       .esHomogeneo should be (false)
   }
 
@@ -18,5 +20,17 @@ class GrupoDepartamentosSpec extends UnitSpec {
 
   it should "poder decir si no incluye una provincia" in {
     departamentosCaba.incluyeA("Santa Fe") should be (false)
+  }
+
+  it should "poder ser usado como estadistica" in {
+    Registro(new LocalDate(2013, 12, 30), 70000, 50000, Departamento("Flores", "CABA"))
+    Registro(new LocalDate(2014, 12, 30), 100000, 80000, Departamento("Almagro", "CABA"))
+    Registro(new LocalDate(2014, 12, 30), 40000, 30000, Departamento("Palermo", "CABA"))
+
+    departamentosCaba.totalVentas should be(170000)
+  }
+
+  after {
+    HomeRegistros.clear()
   }
 }
